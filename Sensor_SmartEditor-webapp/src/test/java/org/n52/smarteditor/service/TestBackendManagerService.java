b57545e288144d39f14cadc31309fbbca6d4020b
Jana Klemp <j_klem04@uni-muenster.de>
2014-12-12 18:04:17 +0100

    weiter: lizenzheader, test regex funktioniert, test fuer validierung weiter

--- Sensor_SmartEditor-webapp/src/test/java/org/n52/smarteditor/service/TestBackendManagerService.java
+++ Sensor_SmartEditor-webapp/src/test/java/org/n52/smarteditor/service/TestBackendManagerService.java
@@ -1,3 +1,31 @@
+/**
+ * Copyright (C) ${inceptionYear}-2014 52°North Initiative for Geospatial Open Source
+ * Software GmbH
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as published
+ * by the Free Software Foundation.
+ *
+ * If the program is linked with libraries which are licensed under one of
+ * the following licenses, the combination of the program with the linked
+ * library is not considered a "derivative work" of the program:
+ *
+ *     - Apache License, version 2.0
+ *     - Apache Software License, version 1.0
+ *     - GNU Lesser General Public License, version 3
+ *     - Mozilla Public License, versions 1.0, 1.1 and 2.0
+ *     - Common Development and Distribution License (CDDL), version 1.0
+ *
+ * Therefore the distribution of the program linked with libraries licensed
+ * under the aforementioned licenses, is permitted by the copyright holders
+ * if the distribution is compliant with both the GNU General Public
+ * License version 2 and the aforementioned licenses.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ */
 package org.n52.smarteditor.service;
 
 import java.util.Collection;
@@ -26,49 +54,54 @@ import static org.hamcrest.Matchers.not;
 public class TestBackendManagerService {
 	@Resource(name = "backendSetupService")
 	BackendManagerService backendManagerService;
-	
+
 	@Before
-public void before(){
-		backendManagerService.setMergeDocument(DOMUtil
-				.createDocumentFromSystemID("classpath:/test-sensor.xml", true));
-}
+	public void before() {
+		backendManagerService
+				.setMergeDocument(DOMUtil.createDocumentFromSystemID(
+						"classpath:/test-sensor.xml", true));
+	}
+
 	/**
-	 * Tests if all needed beans are used in mergeBackend. If that is true all 
+	 * Tests if all needed beans are used in mergeBackend. If that is true all
 	 * values "test" have to be overwritten by the beans.
 	 */
 	@Test
 	public void testMergeBackend() {
-		
-		String docString1 = DOMUtil.convertToString(backendManagerService.getMergeDocument()
-				, false);
-		assertThat(docString1,containsString("test"));
-	
+
 		String docString2 = DOMUtil.convertToString(
 				backendManagerService.mergeBackend(), false);
 		assertThat(docString2, not(containsString("test")));
+		
+		String docString1 = DOMUtil.convertToString(
+				backendManagerService.getMergeDocument(), false);
+		assertThat(docString1, containsString("test"));
+
 	}
 
 	@Test
-public void testRegexSML(){
-		
-	for (Map.Entry<String, BaseBean> lEntry : backendManagerService.getBackend().getStorage()
-			.entrySet()) {
-		// check if bean should be merged
-		if (backendManagerService.isBeanActive(lEntry.getKey())) {
-			 assertThat(lEntry.getKey(),containsString("sml"));
-		}
-	}	
-	}
-@Test 
-public void testRegexNOSML(){
-	backendManagerService.setMergeDocument(DOMUtil
-			.createDocumentFromSystemID("classpath:/dataset.xml", true));
-	for (Map.Entry<String, BaseBean> lEntry : backendManagerService.getBackend().getStorage()
-			.entrySet()) {
-		// check if bean should be merged
-		if (backendManagerService.isBeanActive(lEntry.getKey())) {
-			 assertThat(lEntry.getKey(),not(containsString("sml")));
+	public void testRegexSML() {
+
+		for (Map.Entry<String, BaseBean> lEntry : backendManagerService
+				.getBackend().getStorage().entrySet()) {
+			// check if bean should be merged
+			if (backendManagerService.isBeanActive(lEntry.getKey())) {
+				assertThat(lEntry.getKey(), containsString("sml"));
+			}
 		}
 	}
-}
+
+	@Test
+	public void testRegexNOSML() {
+		backendManagerService.setMergeDocument(DOMUtil
+				.createDocumentFromSystemID("classpath:/templates/dataset.xml",
+						true));
+		for (Map.Entry<String, BaseBean> lEntry : backendManagerService
+				.getBackend().getStorage().entrySet()) {
+			// check if bean should be merged
+			if (backendManagerService.isBeanActive(lEntry.getKey())) {
+				assertThat(lEntry.getKey(), not(containsString("sml")));
+			}
+		}
+	}
 }
--- Sensor_SmartEditor-webapp/src/test/java/org/n52/smarteditor/service/TestBackendManagerService2.java
+++ Sensor_SmartEditor-webapp/src/test/java/org/n52/smarteditor/service/TestBackendManagerService2.java
@@ -1,6 +1,42 @@
+/**
+ * Copyright (C) ${inceptionYear}-2014 52°North Initiative for Geospatial Open Source
+ * Software GmbH
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as published
+ * by the Free Software Foundation.
+ *
+ * If the program is linked with libraries which are licensed under one of
+ * the following licenses, the combination of the program with the linked
+ * library is not considered a "derivative work" of the program:
+ *
+ *     - Apache License, version 2.0
+ *     - Apache Software License, version 1.0
+ *     - GNU Lesser General Public License, version 3
+ *     - Mozilla Public License, versions 1.0, 1.1 and 2.0
+ *     - Common Development and Distribution License (CDDL), version 1.0
+ *
+ * Therefore the distribution of the program linked with libraries licensed
+ * under the aforementioned licenses, is permitted by the copyright holders
+ * if the distribution is compliant with both the GNU General Public
+ * License version 2 and the aforementioned licenses.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ */
 package org.n52.smarteditor.service;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.StringReader;
+
 import javax.annotation.Resource;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -22,7 +58,7 @@ public class TestBackendManagerService2 {
 	@Before
 	public void before() {
 		mService.setMergeDocument(DOMUtil.createDocumentFromSystemID(
-				"classpath:/test-sensor.xml", false));
+				"classpath:/test-sensor.xml", true));
 	}
 
 	@Test
@@ -32,6 +68,31 @@ public class TestBackendManagerService2 {
 				mService.mergeBackend(),false);
 		Document lReport = mService.validate("smlValidator");
 		String report = DOMUtil.convertToString(lReport, true);
+		System.out.println("Report"+report);
 	}
+/*	@Test
+	public void testTransformation() {
 
+		 String formattedOutput = "";
+		    try {
+
+		        TransformerFactory tFactory = TransformerFactory.newInstance();            
+		        Transformer transformer =
+		                tFactory.newTransformer( new StreamSource( "//target//test-classes//validation//iso_svrl_for_xslt2TEST.xsl") );
+
+		        StreamSource xmlSource = new StreamSource(new StringReader("//target//test-classes//test-sensor.xml") );
+		        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		        transformer.transform( xmlSource, new StreamResult( baos ) );
+
+		        formattedOutput = baos.toString();
+
+		    } catch( Exception e ) {
+		        e.printStackTrace();
+		    }
+
+	}*/
+	@Test
+	public void testTransformation2(){
+	
+	}
 }
