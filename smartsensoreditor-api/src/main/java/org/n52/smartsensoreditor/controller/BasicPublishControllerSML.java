62687e6ee305c2bb1ab5751c3959b03338367a12
Jana Klemp <j_klem04@uni-muenster.de>
2015-09-22 17:44:08 +0200

    +"/soap" deleted

--- smartsensoreditor-api/src/main/java/org/n52/smartsensoreditor/controller/BasicPublishControllerSML.java
+++ smartsensoreditor-api/src/main/java/org/n52/smartsensoreditor/controller/BasicPublishControllerSML.java
@@ -121,7 +121,7 @@ public class BasicPublishControllerSML extends BasicPublishController {
 		AbstractCatalogService service = getCatalogService();
 		if (service instanceof SOSCatalogService) {
 			SOSCatalogService sosService = (SOSCatalogService) service;
-			sosService.init(lBean.getServiceUrlSOS()+"/soap");
+			sosService.init(lBean.getServiceUrlSOS());
 			sosService.addRequestHeader("Authorization", lBean.getServiceTokenSOS());
 		}
 		
